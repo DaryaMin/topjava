3d27bdc6a1e7211d0b5342f594b7fad12f9a4d48
DariaMin <dashaminako@gmail.com>
2023-01-30 14:30:27 +0300

    remove import NotNull

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.topjava.util;
 
-import com.sun.istack.internal.NotNull;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExcess;
 
