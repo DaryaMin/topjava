94f33919a0868b30aea37fd4bf3eaddfdea65578
DariaMin <dashaminako@gmail.com>
2023-03-27 12:19:39 +0300

    add test getMeals

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -2,14 +2,20 @@ package ru.javawebinar.topjava.web;
 
 import org.assertj.core.matcher.AssertionMatcher;
 import org.junit.jupiter.api.Test;
+import ru.javawebinar.topjava.MatcherFactory;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.to.MealTo;
 
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.*;
+import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
+import static ru.javawebinar.topjava.util.MealsUtil.getTos;
 
 class RootControllerTest extends AbstractControllerTest {
 
@@ -25,6 +31,23 @@ class RootControllerTest extends AbstractControllerTest {
                             @Override
                             public void assertion(List<User> actual) throws AssertionError {
                                 USER_MATCHER.assertMatch(actual, admin, guest, user);
+                            }
+                        }
+                ));
+    }
+    @Test
+    void getMeals() throws Exception {
+        perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals",
+                        new AssertionMatcher<List<MealTo>>() {
+                            @Override
+                            public void assertion(List<MealTo> actual) throws AssertionError {
+                                List<MealTo> mealsTo = getTos(meals, DEFAULT_CALORIES_PER_DAY);
+                                MatcherFactory.usingIgnoringFieldsComparator(MealTo.class, "user").assertMatch(actual, mealsTo);
                             }
                         }
                 ));
